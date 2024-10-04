fd5487dbb1f0ee754ba45ac09f731abd54286c76
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-10-04 16:07:15 +0000

    improved naming

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -x
+
 SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$(basename $DIR)
 
 VERSION=$(cat version.txt)
 echo "Building peecs version $VERSION"
 
-echo  "$CONTAINER_TOKEN" | docker login -u "$CONTAINER_USERNAME" --password-stdin
-docker build -f peecs/Dockerfile --no-cache --progress=plain -t ${CONTAINER_USERNAME}/peecs:$VERSION peecs
-docker push ${CONTAINER_USERNAME}/peecs:$VERSION
+echo  "$REGISTRY_TOKEN" | docker login -u "$REGISTRY_USERNAME" --password-stdin
+docker build -f peecs/Dockerfile --no-cache --progress=plain -t ${REGISTRY_USERNAME}/peecs:$VERSION peecs
+docker push ${REGISTRY_USERNAME}/peecs:$VERSION
